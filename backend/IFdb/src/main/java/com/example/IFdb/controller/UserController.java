20aef5863215bc9338f9d70489f0a0eab39b3197
YordanAngelov99 <80767413+YordanAngelov99@users.noreply.github.com>
2021-05-22 15:25:35 +0300

    Add register user functionality

--- backend/IFdb/src/main/java/com/example/IFdb/controller/UserController.java
+++ backend/IFdb/src/main/java/com/example/IFdb/controller/UserController.java
@@ -1,19 +1,39 @@
 package com.example.IFdb.controller;
 
 
+import com.example.IFdb.model.dto.InputUserDto;
+import com.example.IFdb.model.dto.UserDto;
+import com.example.IFdb.model.entity.User;
 import com.example.IFdb.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/users")
+@Validated
 public class UserController {
     private UserService userService;
+    private ModelMapper modelMapper = new ModelMapper();
 
     @Autowired
     public UserController(UserService userService){
         this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDto> registerUser(@Valid @RequestBody InputUserDto inputUserDto){
+        User newUser = this.userService.registerUser(inputUserDto);
+        return new ResponseEntity<>(this.modelMapper.map(newUser,UserDto.class), HttpStatus.CREATED);
     }
 
 
--- backend/IFdb/src/main/java/com/example/IFdb/service/UserService.java
+++ backend/IFdb/src/main/java/com/example/IFdb/service/UserService.java
@@ -1,5 +1,9 @@
 package com.example.IFdb.service;
 
 
+import com.example.IFdb.model.dto.InputUserDto;
+import com.example.IFdb.model.entity.User;
+
 public interface UserService {
+    User registerUser(InputUserDto user);
 }
